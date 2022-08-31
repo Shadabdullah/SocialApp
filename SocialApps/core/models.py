c2d010e1e44383ae3922691acab6ad1880802b49
shadAbdullah <shadabdullah101@gmail.com>
2022-08-31 21:58:40 +0530

    lines added

--- SocialApps/core/models.py
+++ SocialApps/core/models.py
@@ -1,3 +1,16 @@
+from email.policy import default
+import imp
+from statistics import mode
 from django.db import models
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Create your models here.
+
+class Profile(models.Model):
+    id_user = pass
+    pasword = pass
+    bio = pass
+    profileimage = models.ImageField(upload_to ="profile_image",default=)
+    location = pass
+
